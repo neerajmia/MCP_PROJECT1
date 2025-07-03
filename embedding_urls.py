e8700ef4affe11bfc6d0a716ce699fab20b92f39
neerajmia <neeraj.shukla44@gmail.com>
2025-07-03 16:43:00 -0700

    Update embedding_urls.py

--- embedding_urls.py
+++ embedding_urls.py
@@ -3,11 +3,5 @@ from sentence_transformers import SentenceTransformer, util
 embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
 #embedding_url = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
 llm_url = "https://api-inference.huggingface.co/models/google/flan-t5-base"
-#   llm_url = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.2"
-
-#model_repo_id= "sentence-transformers/all-MiniLM-L6-v2"
-
-model_repo_id= "google/flan-t5-base"
-
 embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
